82c7d737ac32fe5c108b2e696a297758b5ae038a
jinoleynik <jinoleynik@gmail.com>
2019-05-31 16:38:16 +0300

    Add files via upload

---
+++ precache-manifest.37af90942a11bc74a391f90923fd477c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3dcf9cc0397b6c0b524c",
+    "url": "/static/js/runtime~main.0395b3f1.js"
+  },
+  {
+    "revision": "a279bd055c10bac8edcb",
+    "url": "/static/js/main.2a48db27.chunk.js"
+  },
+  {
+    "revision": "0b825f29b64f583ac200",
+    "url": "/static/js/3.b08cb18f.chunk.js"
+  },
+  {
+    "revision": "2981d3288b466cca4f75",
+    "url": "/static/js/2.4993c008.chunk.js"
+  },
+  {
+    "revision": "a279bd055c10bac8edcb",
+    "url": "/static/css/main.92f29bb4.chunk.css"
+  },
+  {
+    "revision": "081e5e75f8c89196aa28767e12b8f046",
+    "url": "/index.html"
+  }
+];
